120fc9830cc9c347b471918635caf4d173efceb8
Gitanshu Talwar <gitanshutalwar@Gitanshus-MacBook-Air.local>
2024-09-30 13:51:42 +0530

    feat:added link in projects

--- app/components/homepage/projects/project-card.jsx
+++ app/components/homepage/projects/project-card.jsx
@@ -16,7 +16,7 @@ function ProjectCard({ project }) {
           <div className="h-2 w-2 lg:h-3 lg:w-3 rounded-full bg-orange-400"></div>
           <div className="h-2 w-2 lg:h-3 lg:w-3 rounded-full bg-green-200"></div>
         </div>
-        <a href={project.code} target="_blank" className="text-center ml-3 w-full text-[#16f2b3] text-base lg:text-xl">
+        <a href={project.code} target="_blank" className="block text-center ml-3 w-full text-[#16f2b3] text-base lg:text-xl">
           {project.name}
         </a>
       </div>
